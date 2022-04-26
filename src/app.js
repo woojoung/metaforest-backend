1f6013e70b6be88127bd55f1cad35b8973946702
Woojoung <chae3229@gmail.com>
2022-04-26 17:53:22 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 const passportConfig = require('./passport/index');
 const passport = require('passport');
 
-const MySQLStore = require('express-mysql-session')(session);
+// const MySQLStore = require('express-mysql-session')(session);
 
 
 // load database
