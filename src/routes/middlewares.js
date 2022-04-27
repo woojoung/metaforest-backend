2d9626609220d40d34d5c02c712674718dc93b9a
Woojoung <chae3229@gmail.com>
2022-04-27 16:55:11 +0900

    Update middlewares.js

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -1,4 +1,4 @@
-exports.isLoggedIn = (req, res, next) => {
+export function isLoggedIn(req, res, next) {
     console.log('isLoggedIn req.isAuthenticated : ', req.isAuthenticated())
     console.log('isLoggedIn req.user : ', req.user)
     if (req.isAuthenticated()) {
@@ -7,9 +7,9 @@ exports.isLoggedIn = (req, res, next) => {
         // res.status(200).send('eErrCode 403: Login first'); // eErrCode 403
         res.status(200).json({ status: 403, errCode: 403, message: "Login first"}); // eErrCode 403
     }
-};
+}
 
-exports.isNotLoggedIn = (req, res, next) => {
+export function isNotLoggedIn(req, res, next) {
     console.log('isNotLoggedIn req.isAuthenticated : ', req.isAuthenticated())
     console.log('isNotLoggedIn req.user : ', req.user)
     if (!req.isAuthenticated()) {
@@ -18,4 +18,4 @@ exports.isNotLoggedIn = (req, res, next) => {
         // res.status(200).send('eErrCode 401: Logout first'); // eErrCode 401
         res.status(200).json({ status: 401, errCode: 401, message: "Logout first"}); // eErrCode 401
     }
-};
+}
