12aac89775f8aa8dbe1599efc854f1ba80ee8b60
woojoung <chae3229@gmail.com>
2022-05-02 23:17:09 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -62,12 +62,14 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 // session 불러오기 : 요청 시 개인의 저장공간을 만들어준다.
 app.use(session({
     resave: false, // 세션이 수정되지 않아도 항상 저장할지 확인하는 옵션
-    saveUninitialized: false, // 세션이 unInitialized 상태로 미리 만들어서 저장하는지 묻는 옵션
+    saveUninitialized: true, // 세션이 unInitialized 상태로 미리 만들어서 저장하는지 묻는 옵션
     secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true, // 항상 true(자바스크립트로 진입 불가)
         secure: false,
         maxAge: 1000 * 60 * 60 * 1, // 1시간 유지
+        sameSite: 'none',
+        domain: 'amazonaws.com'
     },
     // rolling: true,
     name: 'meta_sid'
