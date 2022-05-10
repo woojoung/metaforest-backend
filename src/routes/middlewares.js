4975d08c95a2b9fb8a4f8efffdebb7ada494f51c
Woojoung <chae3229@gmail.com>
2022-05-10 18:25:50 +0900

    Update middlewares.js

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -4,8 +4,8 @@ exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next(); // 로그인 상태면 다음 미들웨어 호출
     } else {
-        // res.status(200).send('eErrCode 403: Login first'); // eErrCode 403
-        res.status(200).json({ status: 403, errCode: 403, message: "Login first"}); // eErrCode 403
+        // res.status(200).send({ status: 403, errCode: 403, message: "Login first"});
+        res.status(200).json({ status: 403, errCode: 403, message: "Login first"});
     }
 };
 
@@ -15,7 +15,7 @@ exports.isNotLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         next();
     } else {
-        // res.status(200).send('eErrCode 401: Logout first'); // eErrCode 401
-        res.status(200).json({ status: 401, errCode: 401, message: "Logout first"}); // eErrCode 401
+        // res.status(200).send({ status: 401, errCode: 401, message: "Logout first"});
+        res.status(200).json({ status: 401, errCode: 401, message: "Logout first"});
     }
 };
